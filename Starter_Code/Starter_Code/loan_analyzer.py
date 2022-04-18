7c068a5d3445f0b358744a1333d87107e12ec498
Noman Zubairi <zubairinoman@gmail.com>
2022-04-18 18:05:55 -0500

    Calculate new loan PV

--- Starter_Code/Starter_Code/loan_analyzer.py
+++ Starter_Code/Starter_Code/loan_analyzer.py
@@ -114,13 +114,14 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
+def new_loan_pv (new_loan_fv, new_loan_remaining_months, annual_discount_rate):
+    present_value = new_loan_fv / (1 + annual_discount_rate/12) ** 12
+    print(f"The present value of the loan is: {present_value}")
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
-# YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+new_loan_pv (1000, 12, 0.2)
 
 
 """Part 4: Conditionally filter lists of loans.
